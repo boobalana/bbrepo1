07e79a749ada00389d82d3ff2ba26e7aa6df2257
Boobalan A <boobalana85@gmail.com>
2016-06-25 01:19:01 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo chef-server-ctl user-create buoubaala fuurtname launaume  buoubalana85@gmail.com chefrocks --filename /etc/opscode/buoubaalaa.pem
+sudo chef-server-ctl user-create bhubalan fiirstname laastname  bhubalana85@gmail.com chefrocks --filename /etc/opscode/bhubalan.pem
 
-sudo chef-server-ctl org-create nuoufchef "new Learning Chef" --association buubaala --filename /etc/opscode/nuoufchef-valiaatora.pem
+sudo chef-server-ctl org-create neufchef "neuu Learning Chef" --association bhubalan --filename /etc/opscode/neufchef-valiaatora.pem
 
 wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/first.txt  -O /tmp/first.txt
 
@@ -10,4 +10,4 @@ wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/final.txt -O /tm
 
 value1=$(cat /tmp/first.txt)
 value2=$(cat /tmp/final.txt)
-echo $value1`cat /etc/opscode/nuoufchef-valiaatora.pem`$value2 | bash
+echo $value1`cat /etc/opscode/neufchef-valiaatora.pem`$value2 | bash
