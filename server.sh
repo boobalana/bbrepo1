5b7258fc13c73f60cd02f7443d1158da272899d9
Boobalan A <boobalana85@gmail.com>
2016-06-25 00:48:30 +0530

    script
    script

---
+++ server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo chef-server-ctl user-create buoubaala fuurtname launaume  buoubalana85@gmail.com chefrocks --filename /etc/opscode/buoubaalaa.pem
+
+sudo chef-server-ctl org-create nuoufchef "new Learning Chef" --association buubaala --filename /etc/opscode/nuoufchef-valiaatora.pem
+
+wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/first.txt  -O /tmp/first.txt
+
+wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/final.txt -O /tmp/final.txt
+
+value1=$(cat /tmp/first.txt)
+value2=$(cat /tmp/final.txt)
+echo $value1`cat /etc/opscode/nuoufchef-valiaatora.pem`$value2 | bash
