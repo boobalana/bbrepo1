64d6c602b3490d90fc775150cad63acb00865ec4
Boobalan A <boobalana85@gmail.com>
2016-06-26 01:22:47 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-ls -al /etc/opscode/
-chmod 755 /etc/opscode/pivotal.pem
+sleep 700s
+ls -alR /etc/opscode
+sudo chmod 755 /etc/opscode/pivotal.pem
 sleep 120s 
-sudo /usr/bin/chef-server-ctl user-create bhubalan fiirstname laastname bhubalana85@gmail.com chefrocks --filename /etc/opscode/bhubalan.pem
+sudo /usr/bin/chef-server-ctl user-create bhbalan fiirstname laastname bhubalana85@gmail.com chefrocks --filename /etc/opscode/bhubalan.pem
 sleep 120s
-sudo /usr/bin/chef-server-ctl org-create neufchef "neuu Learning Chef" --association bhubalan --filename /etc/opscode/neufchef-valiaatora.pem
+sudo /usr/bin/chef-server-ctl org-create neufchef "neuu Learning Chef" --association bhbalan --filename /etc/opscode/neufchef-valiaatora.pem
 sleep 120s
 wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/first.txt  -O /tmp/first.txt
 wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/final.txt -O /tmp/final.txt
 
-value1=$(cat /tmp/first.txt)
-value2=$(cat /tmp/final.txt)
+sudo export value1=$(cat /tmp/first.txt)
+sudo export value2=$(cat /tmp/final.txt)
 sleep 120s
 echo $value1`cat /etc/opscode/neufchef-valiaatora.pem`$value2 | bash
