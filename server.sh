a01ed24605d1d5215c180f7f1f7fc62541d0b0ca
Boobalan A <boobalana85@gmail.com>
2016-06-26 03:34:29 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-sleep 600s
+sleep 300s
 ls -alR /etc/opscode
 sudo chmod 755 /etc/opscode/pivotal.pem
-sleep 60s 
 sudo /usr/bin/chef-server-ctl user-create bhbalan firstname lastname boobalana85@gmail.com chefrocks --filename /etc/opscode/bhubalan.pem
 sudo /usr/bin/chef-server-ctl org-create neufchef "neuu Learning Chef" --association bhbalan --filename /etc/opscode/neufchef-valiaatora.pem
 wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/first.txt  -O /tmp/first.txt
 wget https://raw.githubusercontent.com/boobalana/bbrepo1/master/final.txt -O /tmp/final.txt
-
 value1=`cat /tmp/first.txt`
 value2=`cat /tmp/final.txt`
-sleep 20s
 echo $value1`cat /etc/opscode/neufchef-valiaatora.pem`$value2 | bash
