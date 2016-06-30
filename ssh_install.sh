210a52265e1ac2283dac93d4fe52dd9948352e8a
Boobalan A <boobalana85@gmail.com>
2016-07-01 00:55:10 +0530

    updated scripts
    scripts updated with curl command

--- ssh_install.sh
+++ ssh_install.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-##Checking availability of file pivotal.pem before creating organisation and user. 
-sleep 100s
-sudo wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa.pub -O /home/adminuser/.ssh/id_rsa.pub 
-sudo cat /home/adminuser/.ssh/id_rsa.pub >> /home/adminuser/.ssh/authorized_keys
-##pull files from repo  
-wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/config  -O /home/adminuser/.ssh/config
+curl https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa.pub -o /home/adminuser/.ssh/authorized_keys
