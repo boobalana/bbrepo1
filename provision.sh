74a65e8357921a781a2aeaaffbd0bce14d1bf6da
Boobalan A <boobalana85@gmail.com>
2016-07-01 03:41:53 +0530

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/config -O /home/adminuser/.ssh/config
-curl https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa -O /home/adminuser/.ssh/id_rsa
+wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/config -O /home/adminuser/.ssh/config
+wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa -O /home/adminuser/.ssh/id_rsa
+chmod 600 /home/adminuser/.ssh/id_rsa
+wget https://github.com/boobalana/bbrepo1/blob/delivery-branch/delivery.license -O /home/adminuser/delivery.license
