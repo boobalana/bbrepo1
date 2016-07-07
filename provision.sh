61a9d03d783569d433f1a5e99387b6abe163b264
Boobalan A <boobalana85@gmail.com>
2016-07-07 20:05:22 +0530

    Delete provision.sh

--- provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/config -O /home/adminuser/.ssh/config
-wget https://raw.githubusercontent.com/boobalana/bbrepo1/delivery-branch/id_rsa -O /home/adminuser/.ssh/id_rsa
-chmod 600 /home/adminuser/.ssh/id_rsa
-wget https://github.com/boobalana/bbrepo1/blob/delivery-branch/delivery.license -O /home/adminuser/delivery.license
