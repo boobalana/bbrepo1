d7578cd9664467c3c4fd1fec7916f1665bca88cf
Boobalan A <boobalana85@gmail.com>
2016-08-01 16:19:35 +0530

    updated bisotti value
    updated bisotti value

--- configure.sh
+++ configure.sh
@@ -2,5 +2,6 @@
 FQDN=`hostname -f`
 echo "api_fqdn '$FQDN'" | sudo tee -a /etc/chef-marketplace/marketplace.rb
 sudo sed -i "s/^fqdn .*/fqdn '$FQDN'/" /etc/chef-compliance/chef-compliance.rb
+sed -i "s/^biscotti.*$/biscotti.enabled = false/"  /etc/chef-marketplace/marketplace.rb
 sudo chef-marketplace-ctl hostname $FQDN
 sudo chef-compliance-ctl reconfigure
